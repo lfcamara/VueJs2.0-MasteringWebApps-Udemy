5f6878cc2001ed4dd6760d443920b9f71d49c4ef
unknown <leo.camara@gmail.com>
2020-09-01 20:32:25 -0300

    Adding application 4

---
+++ Section4 - Starbase/starbase/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <div class="col-md-12">
+            <Character
+                v-for="(id, index) in initial_ids"
+                :key="index"
+                :id="id"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import Character from "./components/Character.vue"
+
+export default {
+    name: "app",
+    data(){
+        return {
+            title: "Generate your team",
+            initial_ids: [1, 2, 3]
+        }
+    },
+    components: {
+        Character
+    }
+}
+</script>
